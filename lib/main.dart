8419b6d29bae808975e42a3c6721319b329886c7
Jordan <jojobonnot@gmail.com>
2020-12-02 14:39:00 +0100

    Progress bar

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,7 @@ class _SignupFormState extends State<SignupForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -97,7 +98,7 @@ class _SignupFormState extends State<SignupForm> {
                 return states.contains(MaterialState.disabled) ? null : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: Text("Sign up !"),
           ),
         ],
@@ -108,5 +109,25 @@ class _SignupFormState extends State<SignupForm> {
 
   void _showWelcomeScreen() {
     Navigator.of(context).pushNamed("/welcome");
+  }
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+
+    var controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _userNameTextController
+    ];
+
+    for (var controller in controllers) {
+      if(controller.value.text.isNotEmpty){
+        progress += 1 / controllers.length;
+      }
+    }
+    
+    setState(() {
+      _formProgress = progress;
+    });
   }
 }
